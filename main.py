81522c708de2c8acc15cf490c9dd3ab730cd7501
Carolyn Whelpley <33427006+mcdonaldcarolyn@users.noreply.github.com>
2024-01-25 18:53:08 +0000

    create start of clues

--- main.py
+++ main.py
@@ -25,4 +25,26 @@ liars = ramdom.sample(SUSPECTS, random.randint(3,4))
 accusationsLeft = 3
 culprit = random.choice(SUSPECTS)
 
+random.shufffle(SUSPECTS)
+random.shuffle(ITEMS)
+random.shuffle(PLACES)
+
+clues = {}
+for i, interviewee in enumerate(SUSPECTS):
+    if interviewee in liars:
+        continue
+    
+    clues[interviewee] = {}
+    clues[interviewee] ['debug_liar'] = False
+    for item in ITEMS: 
+        if random.randint(0,1) == 0:
+            clues[interviewee][item] = PLACES[ITEMS.index(item)]
+        else:
+            clues[interviewee][item] = SUSPECTS[ITEMS(item)]
+    for suspect in SUSPECTS:
+        if random.randint(0,1) == 0:
+            clues[interviewee][suspect] = PLACES[SUSPECTS.index(suspect)]
+        else:
+            clues[interviewee][suspect] = ITEMS[SUSPECTS.index(suspect)]
+            
 
